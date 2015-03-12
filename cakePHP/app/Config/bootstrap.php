395b082d9a499ce23f3cd5e2d5fb4a53e826713d
camieac <camieac@gmail.com>
2015-03-12 14:08:35 +0000

    Added bootstrap stuff

--- cakePHP/app/Config/bootstrap.php
+++ cakePHP/app/Config/bootstrap.php
@@ -110,3 +110,20 @@ CakeLog::config('error', array(
 	'file' => 'error',
 ));
 CakePlugin::load('FullCalendar');
+
+App::uses('CakeEventManager', 'Event');
+App::uses('FileStorageUtils', 'FileStorage.Lib/Utility');
+App::uses('StorageManager', 'FileStorage.Lib');
+App::uses('LocalImageProcessingListener', 'FileStorage.Event');
+App::uses('LocalFileStorageListener', 'FileStorage.Event');
+App::uses('FileStorageListener', 'FileStorage.Event');
+
+// Only required if you're *NOT* using composer or another autoloader!
+spl_autoload_register(__NAMESPACE__ .'\FileStorageUtils::gaufretteLoader');
+
+$listener = new LocalFileStorageListener();
+CakeEventManager::instance()->attach($listener);
+
+// For automated image processing you'll have to attach this listener as well
+$listener = new ImageProcessingListener();
+CakeEventManager::instance()->attach($listener);
