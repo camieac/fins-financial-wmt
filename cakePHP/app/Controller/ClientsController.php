33cb8ca096403b695a30d7f6f3a681c30dd6525c
Cameron Craig <camieac@gmail.com>
2015-03-12 21:07:34 +0000

    Moved uploadImage function

--- cakePHP/app/Controller/ClientsController.php
+++ cakePHP/app/Controller/ClientsController.php
@@ -33,6 +33,55 @@ $this->redirect(array('controller' => 'users', 'action' => 'login'));
 }
 }
 
+public function uploadImage() {
+
+    // Custom
+    $folderToSaveFiles = WWW_ROOT . 'img/profile_pictures/' ;
+
+
+
+
+    if (!$this->request->is('post')) return;        // Not a POST data!
+
+
+    if(!empty($this->request->data))
+    {
+        //Check if image has been uploaded
+        if(!empty($this->request->data['Image']['profileImage']))
+        {
+                $file = $this->request->data['Image']['profileImage']; //put the data into a var for easy use
+
+                debug( $file );
+
+                $ext = substr(strtolower(strrchr($file['name'], '.')), 1); //get the extension
+                $arr_ext = array('png', 'jpeg', 'jpg'); //set allowed extensions
+
+                //only process if the extension is valid
+                if(in_array($ext, $arr_ext))
+                {
+
+
+                    //do the actual uploading of the file. First arg is the tmp name, second arg is 
+                    //where we are putting it
+                    $newFilename = $this->request->data['nis'];//$file['name']; // edit/add here as you like your new filename to be.
+                    $result = move_uploaded_file( $file['tmp_name'], $folderToSaveFiles . $newFilename );
+
+                    debug( $result );
+
+                    //prepare the filename for database entry (optional)
+                    //$this->data['Image']['image'] = $file['name'];
+                }
+        }
+
+        //now do the save (optional)
+        //if($this->Image->save($this->data)) {...} else {...}
+    }
+
+
+
+
+}
+
 public function view($id = null) {
 	
 $this->set('listofstocks', $this->Client->getStockNames());
@@ -202,53 +251,6 @@ __('The Stock with id: %s has been deleted.', h($id))
 		return $stocks;
 	}
 
-public function uploadImage() {
 
-    // Custom
-    $folderToSaveFiles = WWW_ROOT . 'img/profile_pictures/' ;
-
-
-
-
-    if (!$this->request->is('post')) return;        // Not a POST data!
-
-
-    if(!empty($this->request->data))
-    {
-        //Check if image has been uploaded
-        if(!empty($this->request->data['Image']['profileImage']))
-        {
-                $file = $this->request->data['Image']['profileImage']; //put the data into a var for easy use
-
-                debug( $file );
-
-                $ext = substr(strtolower(strrchr($file['name'], '.')), 1); //get the extension
-                $arr_ext = array('png', 'jpeg', 'jpg'); //set allowed extensions
-
-                //only process if the extension is valid
-                if(in_array($ext, $arr_ext))
-                {
-
-
-                    //do the actual uploading of the file. First arg is the tmp name, second arg is 
-                    //where we are putting it
-                    $newFilename = $this->request->data['nis'];//$file['name']; // edit/add here as you like your new filename to be.
-                    $result = move_uploaded_file( $file['tmp_name'], $folderToSaveFiles . $newFilename );
-
-                    debug( $result );
-
-                    //prepare the filename for database entry (optional)
-                    //$this->data['Image']['image'] = $file['name'];
-                }
-        }
-
-        //now do the save (optional)
-        //if($this->Image->save($this->data)) {...} else {...}
-    }
-
-
-
-
-}
 
 } ?>
