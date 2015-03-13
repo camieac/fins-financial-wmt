bf47006bd0064bcbd0e23db0533abc8b7a639d56
camieac <camieac@gmail.com>
2015-03-13 08:16:45 +0000

    Fixed syntax error

--- cakePHP/app/View/Helper/RssHelper.php
+++ cakePHP/app/View/Helper/RssHelper.php
@@ -5,7 +5,7 @@ class RssHelper extends AppHelper {
 
     function parseRss($limit = 3) {
         // Parse the RSS feed
-        $xml = Xml::build("http://feeds.bbci.co.uk/news/business/rss.xml');
+        $xml = Xml::build('http://feeds.bbci.co.uk/news/business/rss.xml');
         $data = $xml->toArray();
 
         // Filter any non-news items
